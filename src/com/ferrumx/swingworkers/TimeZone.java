0b45c8f41064812dada748ddddfcdf5c387bbc4c
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-12-12 13:00:42 +0530

    SwingWorker for TimeZone

---
+++ src/com/ferrumx/swingworkers/TimeZone.java
@@ -0,0 +1,44 @@
+package com.ferrumx.swingworkers;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import javax.swing.JTextField;
+import javax.swing.SwingWorker;
+
+import com.ferrumx.exceptions.ShellException;
+import com.ferrumx.system.operating_system.Win32_TimeZone;
+
+public class TimeZone extends SwingWorker<Map<String, String>, Void> {
+	
+	private List<JTextField> timeZoneFields;
+	
+	public TimeZone (List<JTextField> timeZoneFields) {
+		this.timeZoneFields = timeZoneFields;
+	}
+
+	@Override
+	protected Map<String, String> doInBackground() throws IndexOutOfBoundsException, IOException, ShellException, InterruptedException {
+		return Win32_TimeZone.getOSTimeZone();
+	}
+	
+	@Override
+	protected void done() {
+		try {
+			Map<String, String> timeZone = get();
+			
+			timeZoneFields.get(2).setText(timeZone.get("StandardName"));
+			timeZoneFields.get(3).setText(timeZone.get("Caption"));
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Thread.currentThread().interrupt();
+		}
+	}
+
+}
