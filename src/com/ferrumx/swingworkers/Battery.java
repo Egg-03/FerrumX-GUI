8206c628ff96689a54fa4b9a28a1841a4b24e3d9
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2025-04-29 15:48:28 +0530

    Revert "Added Maven Nature"
    This reverts commit 0a0332f5aef8293230169a911e21712779e7c07c.

---
+++ src/com/ferrumx/swingworkers/Battery.java
@@ -0,0 +1,139 @@
+package com.ferrumx.swingworkers;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingWorker;
+
+import org.tinylog.Logger;
+
+import com.ferrumx.exceptions.ShellException;
+import com.ferrumx.system.hardware.Win32_Battery;
+import com.ferrumx.system.hardware.Win32_PortableBattery;
+import com.ferrumx.ui.secondary.ExceptionUI;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+
+public class Battery extends SwingWorker<List<Map<String, String>>, Void> {
+	
+	private JLabel batteryChargePercentage;
+	private JLabel batteryChargeIcon;
+	List<JTextField> batteryFields;
+	
+	public Battery(JLabel batteryChargePercentage, JLabel batteryChargeIcon, List<JTextField> batteryFields) {
+		this.batteryChargePercentage = batteryChargePercentage;
+		this.batteryChargeIcon = batteryChargeIcon;
+		this.batteryFields = batteryFields;
+	}
+
+	@Override
+	protected List<Map<String, String>> doInBackground() throws IndexOutOfBoundsException, IOException, ShellException, InterruptedException {
+		
+		Map<String, String> battery = Win32_Battery.getBattery();
+		Map<String, String> portableBattery = Win32_PortableBattery.getPortableBattery();
+		
+		return List.of(battery, portableBattery);
+	}
+	
+	@Override
+	protected void done() {
+		try {
+			List<Map<String, String>> batteryClasses = get();
+			
+			Map<String, String> battery = batteryClasses.getFirst();
+			Map<String, String> portableBattery = batteryClasses.getLast();
+			
+			if(!battery.isEmpty()) {
+				String batteryCharge = battery.get("EstimatedChargeRemaining");
+				batteryFields.get(0).setText(battery.get("Caption"));
+				batteryFields.get(1).setText(battery.get("Status"));
+				batteryFields.get(2).setText(batteryStatusInterpreter(battery.get("BatteryStatus")));
+				batteryFields.get(3).setText(batteryChemistryInterpreter(battery.get("Chemistry")));
+				batteryFields.get(4).setText(batteryCharge+"%");
+				batteryFields.get(5).setText(battery.get("EstimatedRunTime")+" min.");
+				
+				batteryImageUpdateByCharge(batteryCharge, batteryChargeIcon);
+				batteryChargePercentage.setText("Current Charge Level: "+batteryCharge+"%");
+			}
+			
+			if (!portableBattery.isEmpty()) {
+				batteryFields.get(6).setText(portableBattery.get("Name"));
+				batteryFields.get(7).setText(portableBattery.get("DeviceID"));
+				batteryFields.get(8).setText(portableBattery.get("DesignCapacity")+"mWh");
+				batteryFields.get(9).setText(portableBattery.get("DesignVoltage")+ "mV");
+			}
+			
+		} catch (ExecutionException e) {
+			new ExceptionUI("Battery Error", e.getMessage()+"\nPlease refer to the logs for more information.");
+			Logger.error(e);
+		} catch (InterruptedException e) {
+			new ExceptionUI("Battery Error", e.getMessage()+"\nPlease refer to the logs for more information.");
+			Logger.error(e);
+			Thread.currentThread().interrupt();
+		}
+	}
+	
+	private String batteryStatusInterpreter (String charge) {
+		switch (charge) {
+		case "1":
+			return "Discharging";
+		case "2":
+			return "Plugged In";
+		case "3":
+			return "Fully Charged";
+		case "4":
+			return "Low";
+		case "5":
+			return "Critical";
+		case "6":
+			return "Charging";
+		case "7":
+			return "Charging and High";
+		case "8":
+			return "Charging and Low";
+		case "9":
+			return "Charging and Critical";
+		case "10":
+			return "Undefined";
+		case "11":
+			return "Partially Charged";
+		default:
+			return charge;
+		}
+	}
+	
+	private String batteryChemistryInterpreter (String chemistry) {
+		switch (chemistry) {
+		case "1":
+			return "Other";
+		case "2":
+			return "Unknown";
+		case "3":
+			return "Lead Acid";
+		case "4":
+			return "Nickel Cadmium";
+		case "5":
+			return "Nickel MetalHydride";
+		case "6":
+			return "Lithium-ion";
+		case "7":
+			return "Zinc Air";
+		case "8":
+			return "Lithium Polymer";
+		default:
+			return chemistry;
+		}
+	}
+	
+	private void batteryImageUpdateByCharge(String currentCharge, JLabel batteryChargeIcon) {
+		Integer charge = Integer.valueOf(currentCharge);
+		Integer ceilCharge = Math.ceilDiv(charge,10)*10; // Round to the ceiling value of the nearest multiple of 10
+		String currentBatteryIndicator = String.valueOf(ceilCharge);
+		
+		batteryChargeIcon.setIcon(new FlatSVGIcon(Battery.class.getResource("/resources/battery_level_images/Battery-"+currentBatteryIndicator+".svg")));
+	}
+
+}
---
+++ src/com/ferrumx/ui/secondary/ExceptionUI.java
@@ -0,0 +1,79 @@
+package com.ferrumx.ui.secondary;
+
+import java.awt.Color;
+import java.awt.Desktop;
+import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+public class ExceptionUI extends JFrame {
+	private static final long serialVersionUID = 5951705399700376822L;
+	private JTextArea exceptionArea = new JTextArea();
+
+	public ExceptionUI(String errorName, String errorMessage) {
+		super("Crash Report Engine");
+		initialize(errorName, errorMessage);
+	}
+
+	private void initialize(String errorName, String errorMessage) {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(ExceptionUI.class.getResource("/resources/icon_main.png")));
+		setResizable(false);
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		setSize(490, 190);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(null);
+
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(
+				new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), errorName,
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(10, 11, 454, 130);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		exceptionArea.setLineWrap(true);
+		exceptionArea.setForeground(new Color(0, 128, 128));
+		exceptionArea.setEditable(false);
+		exceptionArea.setBackground(new Color(32, 32, 32));
+		exceptionArea.setText(errorMessage);
+
+		JScrollPane scrollPane = new JScrollPane(exceptionArea);
+		scrollPane.setBounds(10, 22, 434, 70);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		panel.add(scrollPane);
+
+		JButton openLogFolder = new JButton("Open The Log Folder");
+		openLogFolder.addActionListener( e-> {
+			try {
+				Desktop.getDesktop().open(new File("crashlogs"));
+			} catch (IOException | NullPointerException | IllegalArgumentException | UnsupportedOperationException | SecurityException e1) {
+				SwingUtilities.invokeLater(()->exceptionArea.setText(e1.getMessage()));
+			}
+		});
+		openLogFolder.setBounds(10, 98, 170, 22);
+		panel.add(openLogFolder);
+		
+		JButton checkLatestLogs = new JButton("Check The Latest Log");
+		checkLatestLogs.addActionListener( e-> {
+			try {
+				Desktop.getDesktop().open(new File("crashlogs/latest.log"));
+			} catch (IOException | NullPointerException | IllegalArgumentException | UnsupportedOperationException | SecurityException e1) {
+				SwingUtilities.invokeLater(()->exceptionArea.setText(e1.getMessage()));
+			}
+		});
+		checkLatestLogs.setBounds(284, 98, 160, 22);
+		panel.add(checkLatestLogs);
+		
+		setVisible(true);
+	}
+}
