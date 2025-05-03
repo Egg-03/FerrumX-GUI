f6a231dceb97ee18fc07c5cb530e95769ddf7d5e
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2025-05-03 11:48:30 +0530

    Update application version to 1.2.2
    Bumped the APP_VERSION constant from 1.2.1 to 1.2.2 in VersionAndOtherInfo.java to reflect the latest application version.

--- src/main/java/com/ferrumx/ui/utilities/VersionAndOtherInfo.java
+++ src/main/java/com/ferrumx/ui/utilities/VersionAndOtherInfo.java
@@ -2,7 +2,7 @@ package com.ferrumx.ui.utilities;
 
 public class VersionAndOtherInfo {
 	
-	public static final String APP_VERSION = "1.2.1";
+	public static final String APP_VERSION = "1.2.2";
 	public static final String FERRUMX_VERSION = "1.3.6";
 	
 	
